8c35824b2a83643958eb99d82493149fc827f470
Manjunath Mohan <manjunat.mohan@gmail.com>
2024-07-20 11:58:16 +0530

    themetoggle persist + polish

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,64 +1,22 @@
-function toggleIcon(elId1, elId2) {
-    let el1 = document.getElementById(elId1);
-    let el2 = document.getElementById(elId2);
-    if (el1) {
-        el1.style.display = 'none';
-    }
-    if (el2) {
-        el2.style.display = 'inline';
-    }
-}
-
-function setTheme(theme) {
-    if (theme === "dark") {
-        toggleIcon("dark-toggler", "light-toggler");
-        if (document.body) {
-            document.body.classList.remove('light');
-            document.body.classList.add('dark');
-        }
-    } else if (theme === "light") {
-        toggleIcon("light-toggler", "dark-toggler");
-        if (document.body) {
-            document.body.classList.remove('dark');
-            document.body.classList.add('light');
-        }
-    }
-    localStorage.setItem("theme", theme);
-}
-
 function toggleTheme() {
-    if (localStorage.getItem("theme") === "light") {
-        setTheme("dark");
-    } else if (localStorage.getItem("theme") === "dark") {
-        setTheme("light");
-    }
-}
-
-const siteTheme = document.documentElement.getAttribute("data-default-theme");
-let savedTheme = localStorage.getItem("theme") || siteTheme;
-// adapt based on user os preference
-if (savedTheme == "auto") {
-    savedTheme = "light";
-    if ( window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
-        savedTheme = "dark";
+    if (document.body.className.includes('dark')) {
+        document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
     }
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    setTheme(savedTheme);
-
     const switcher = document.getElementById("theme-switcher");
     if (switcher) {
-        switcher.addEventListener("click", () => {
-            toggleTheme();
-        });
+        switcher.addEventListener("click", () => { toggleTheme(); });
     }
     // listner to check for user's os preference changes
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
-        if (event.matches) {
-            setTheme("dark");
-        } else {
-            setTheme("light");
+        if (event.matches && localStorage.getItem('theme') === 'light') {
+            toggleTheme();
         }
     });
 });
